ce2ac007a426654a81e70b14366e8a93b3acf821
nkrypt <bryanmuschter@gmail.com>
2015-02-26 22:04:13 -0500

    Remove battle fire system

--- toontown/battle/DistributedBattleBase.py
+++ toontown/battle/DistributedBattleBase.py
@@ -80,7 +80,6 @@ class DistributedBattleBase(DistributedNode.DistributedNode, BattleBase):
         self.adjustFsm = ClassicFSM.ClassicFSM('Adjust', [State.State('Adjusting', self.enterAdjusting, self.exitAdjusting, ['NotAdjusting']), State.State('NotAdjusting', self.enterNotAdjusting, self.exitNotAdjusting, ['Adjusting'])], 'NotAdjusting', 'NotAdjusting')
         self.adjustFsm.enterInitialState()
         self.interactiveProp = None
-        self.fireCount = 0
         return
 
     def uniqueBattleName(self, name):
@@ -1506,9 +1505,3 @@ class DistributedBattleBase(DistributedNode.DistributedNode, BattleBase):
 
     def getCollisionName(self):
         return 'enter' + self.lockoutNodePath.getName()
-
-    def setFireCount(self, amount):
-        self.fireCount = amount
-
-    def getFireCount(self):
-        return self.fireCount
