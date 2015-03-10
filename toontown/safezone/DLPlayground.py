556f03a096b175c702176ae5e28d58aac4dbd0af
nkrypt <bryanmuschter@hotmail.com>
2015-03-10 07:59:06 -0400

    Fix Mark's NearFar code

--- toontown/safezone/DLPlayground.py
+++ toontown/safezone/DLPlayground.py
@@ -1,9 +1,6 @@
 from toontown.classicchars import CCharPaths
 from toontown.safezone import Playground
 from toontown.toonbase import TTLocalizer
-from toontown.toonbase import ToontownGlobals
-
-base.camLens.setNearFar(ToontownGlobals.DreamlandCameraNear, ToontownGlobals.DreamlandCameraFar)
 
 class DLPlayground(Playground.Playground):
     def showPaths(self):
