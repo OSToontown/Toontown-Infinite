816dfad36d9e19fce84da4648bf7333c56ceaee0
Markgasus <markgasus@gmail.com>
2015-03-10 02:49:57 -0400

    DLPlayground: Set near and far in DDL This is so the clouds don't fade off screen when you're too far away from them

--- toontown/safezone/DLPlayground.py
+++ toontown/safezone/DLPlayground.py
@@ -1,7 +1,9 @@
 from toontown.classicchars import CCharPaths
 from toontown.safezone import Playground
 from toontown.toonbase import TTLocalizer
+from toontown.toonbase import ToontownGlobals
 
+base.camLens.setNearFar(ToontownGlobals.DreamlandCameraNear, ToontownGlobals.DreamlandCameraFar)
 
 class DLPlayground(Playground.Playground):
     def showPaths(self):
