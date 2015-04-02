6f8c5007174f0bfdebf04d4fc3aebae0d4db9291
encryptable <bryanmuschter@hotmail.com>
2015-04-02 17:56:14 -0400

    toontowngui: Update size of loading bar

--- toontown/toontowngui/ToontownLoadingScreen.py
+++ toontown/toontowngui/ToontownLoadingScreen.py
@@ -50,7 +50,8 @@ class ToontownLoadingScreen:
         self.__count = 0
         self.gui = loader.loadModel('phase_3/models/gui/progress-background.bam')
         self.title = DirectLabel(guiId='ToontownLoadingScreenTitle', parent=self.gui, relief=None, pos=(base.a2dRight/5, 0, 0.235), text='', textMayChange=1, text_scale=0.08, text_fg=(0.03, 0.83, 0, 1), text_align=TextNode.ALeft, text_font=ToontownGlobals.getSignFont())
-        self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(base.a2dLeft+(base.a2dRight/4.95), base.a2dRight-(base.a2dRight/4.95), -0.03, 0.03), pos=(0, 0, 0.15), text='')
+        self.waitBar = DirectWaitBar(guiId='ToontownLoadingScreenWaitBar', parent=self.gui, frameSize=(base.a2dLeft, base.a2dRight, 0, 0.32), pos=(0, 0, 0), text='', color=(0, 0, 0, 0.5), barColor=(0.6, 0.6, 0.6, 0.5))
+        self.waitBar.setTransparency(TransparencyAttrib.MAlpha)
         logoScale = 0.5625  # Scale for our locked aspect ratio (2:1).
         self.logo = OnscreenImage(
             image='phase_3/maps/toontown-logo.png',
@@ -60,13 +61,10 @@ class ToontownLoadingScreen:
         scale = self.logo.getScale()
         # self.logo.setPos(scale[0], 0, -scale[2])
         self.logo.setPos(0, 0, -scale[2])
-        self.toon = None
 
     def destroy(self):
         self.title.destroy()
         self.gui.removeNode()
-        if self.toon:
-            self.toon.delete()
         self.logo.removeNode()
 
     def getTip(self, tipCategory):
@@ -80,18 +78,6 @@ class ToontownLoadingScreen:
         self.__count = 0
         self.__expectedCount = range
         if gui:
-            if base.localAvatarStyle:
-                from toontown.toon import Toon
-                self.toon = Toon.Toon()
-                self.toon.setDNA(base.localAvatarStyle)
-                self.toon.loop('bored', fromFrame=135, toFrame=135)
-                self.toon.getGeomNode().setDepthWrite(1)
-                self.toon.getGeomNode().setDepthTest(1)
-                self.toon.setHpr(205, 0, 0)
-                self.toon.setScale(0.18)
-                self.toon.setPos(base.a2dBottomRight.getX()/1.25, 0, -0.034)
-                self.toon.reparentTo(self.waitBar)
-                self.waitBar['frameSize'] = (base.a2dLeft+(base.a2dRight/8.15), base.a2dRight-(base.a2dRight/2.57), -0.03, 0.03)
             self.title.reparentTo(base.a2dpBottomLeft, LOADING_SCREEN_SORT_INDEX)
             self.title.setPos(0.24, 0, 0.23)
             self.gui.setPos(0, -0.1, 0)
@@ -111,8 +97,6 @@ class ToontownLoadingScreen:
         self.waitBar.reparentTo(self.gui)
         self.title.reparentTo(self.gui)
         self.gui.reparentTo(hidden)
-        if self.toon:
-            self.toon.reparentTo(hidden)
         self.logo.reparentTo(hidden)
         return (self.__expectedCount, self.__count)
 
