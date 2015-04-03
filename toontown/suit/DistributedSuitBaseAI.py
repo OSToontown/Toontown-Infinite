5a5dc24b4da16644aa541181f40dd8699daf029d
Xenofied <Xenofied@users.noreply.github.com>
2015-04-02 20:05:37 -0500

    suit:Fix HP indicator on revived v2.0's

--- toontown/suit/DistributedSuitBaseAI.py
+++ toontown/suit/DistributedSuitBaseAI.py
@@ -116,7 +116,7 @@ class DistributedSuitBaseAI(DistributedAvatarAI.DistributedAvatarAI, SuitBase.Su
 
     def useSkeleRevive(self):
         self.skeleRevives -= 1
-        self.currHP = self.maxHP
+        self.b_setHP(self.maxHP)
         self.reviveFlag = 1
 
     def reviveCheckAndClear(self):
