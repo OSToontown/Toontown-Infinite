32e5009195b1673555a904709bfb30c4f8d9df58
Bryan Muschter <encryptable@users.noreply.github.com>
2015-04-05 22:36:33 -0400

    toonbase: Remove random.choice for the theme

--- toontown/toonbase/ClientStart.py
+++ toontown/toonbase/ClientStart.py
@@ -132,8 +132,7 @@ import TTLocalizer
 from otp.otpbase import OTPGlobals
 OTPGlobals.setDefaultProductPrefix(TTLocalizer.ProductPrefix)
 if base.musicManagerIsValid:
-    themeList = ('phase_3/audio/bgm/tti_theme.ogg', 'phase_3/audio/bgm/tti_theme_2.ogg')
-    music = base.loadMusic(random.choice(themeList))
+    music = base.loadMusic('phase_3/audio/bgm/tti_theme.ogg')
     if music:
         music.setLoop(1)
         music.setVolume(0.9)
