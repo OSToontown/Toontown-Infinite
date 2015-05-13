94dff99df0f42ef8ec000f14ad76fe855ceaca3c
pythonology <pythonology@gmail.com>
2015-05-13 11:22:39 -0400

    Chan...

--- toontown/util/PriorityCallbacks.py
+++ toontown/util/PriorityCallbacks.py
@@ -2,20 +2,24 @@ import bisect
 
 
 class PriorityCallbacks:
+    """ manage a set of prioritized callbacks, and allow them to be invoked in order of priority """
     def __init__(self):
-        self.callbacks = []
+        self._callbacks = []
 
-    def add(self, callback, priority=0):
+    def clear(self):
+        while self._callbacks:
+            self._callbacks.pop()
+
+    def add(self, callback, priority=None):
+        if priority is None:
+            priority = 0
         item = (priority, callback)
-        bisect.insort(self.callbacks, item)
+        bisect.insort(self._callbacks, item)
         return item
 
     def remove(self, item):
-        self.callbacks.pop(bisect.bisect_left(self.callbacks, item))
-
-    def clear(self):
-        self.callbacks = []
+        self._callbacks.pop(bisect.bisect_left(self._callbacks, item))
 
     def __call__(self):
-        for _, callback in self.callbacks:
+        for priority, callback in self._callbacks:
             callback()
