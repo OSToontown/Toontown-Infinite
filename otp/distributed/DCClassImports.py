210d7a90bc23cc93507bffe332881795f85cd289
Chan <chandler14362@gmail.com>
2015-06-01 11:19:09 -0400

    bugfix: Fix chat bubbles not appearing to players that join an area

--- otp/distributed/DCClassImports.py
+++ otp/distributed/DCClassImports.py
@@ -2,7 +2,7 @@
 from pandac.PandaModules import *
 
 
-hashVal = 3894372954L
+hashVal = 3894614450L
 
 
 from toontown.cogdominium import DistributedCogdoInterior, DistributedCogdoBattleBldg, DistributedCogdoElevatorExt, DistributedCogdoElevatorInt, DistributedCogdoBarrel, DistCogdoGame, DistCogdoLevelGame, DistCogdoBoardroomGame, DistCogdoCraneGame, DistCogdoMazeGame, DistCogdoFlyingGame, DistCogdoCrane, DistCogdoCraneMoneyBag, DistCogdoCraneCog
