b17e06c9898cb67739e5cf062caa4fce9d971e5f
David <necromoni@hotmail.com>
2015-07-17 23:09:40 -0600

    Added the option to delete a single sos card

--- otp/distributed/DCClassImports.py
+++ otp/distributed/DCClassImports.py
@@ -2,7 +2,7 @@
 from pandac.PandaModules import *
 
 
-hashVal = 3526889126L
+hashVal = 234338751
 
 
 from toontown.cogdominium import DistributedCogdoInterior, DistributedCogdoBattleBldg, DistributedCogdoElevatorExt, DistributedCogdoElevatorInt, DistributedCogdoBarrel, DistCogdoGame, DistCogdoLevelGame, DistCogdoBoardroomGame, DistCogdoCraneGame, DistCogdoMazeGame, DistCogdoFlyingGame, DistCogdoCrane, DistCogdoCraneMoneyBag, DistCogdoCraneCog
